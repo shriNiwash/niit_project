01ec59a87a4888664046e0c88d6b7b067a902a79
shriNiwash <shriniwashsharma98413@gmail.com>
2022-08-18 20:57:00 +0545

    niit project base

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const PORT = 3000 || process.env.PORT;
+const bodyparser = require('body-parser');
+const hbs = require('hbs');
+const path = require('path');
+
+
+//body-parser configuration
+app.use(bodyparser.json());
+app.use(express.urlencoded({extended:false}));
+
+//static folder configuration
+const staticPath = path.join('__dirname',"../public");
+app.use(express.static(staticPath));
+
+//configuration of view engine
+app.set('view engine','hbs');
+app.set('views','./views');
+
+
+app.get("/",(req,res)=>{
+    res.render('index');
+})
+
+app.get("/about",(req,res)=>{
+    res.render('about');
+})
+
+
+app.listen(PORT,()=>console.log(`localhost://${PORT}`));
